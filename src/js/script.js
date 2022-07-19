1798286d6b204e47572ff8c1d0580eae16d0d567
strannik1105 <danil.stolpovskikh@gmail.com>
2022-07-19 21:12:19 +0700

    Add dropdown list

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,17 @@
-function aaa() {
-    let ff = 1;
-    return 2;
+let buildings_menu = document.querySelector('.header__menu_item_button')
+
+if(buildings_menu)
+{
+    buildings_menu.onclick = function()
+    {
+        let buildings = document.querySelector('.header__menu_buildings');
+        if(buildings.getAttribute('hidden'))
+        {
+            buildings.removeAttribute('hidden');           
+        }
+        else
+        {
+            buildings.setAttribute('hidden', true);
+        }
+    }
 }
